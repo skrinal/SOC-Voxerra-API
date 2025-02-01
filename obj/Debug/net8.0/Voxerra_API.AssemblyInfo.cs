250aed258f944cd316dd8471e8866a1deecee6f4
skrinal <richard.kamenistak@gmail.com>
2025-02-01 21:26:53 +0100

    xd

--- obj/Debug/net8.0/Voxerra_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Voxerra_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Voxerra_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11af6ca9fe0841b46f3d3a80f8e42c5c9df9b0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fb4df0a2d39c7393d860b1ad6469d7634d9936")]
 [assembly: System.Reflection.AssemblyProductAttribute("Voxerra_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Voxerra_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
